508e574f297f109bd22b6aeb629b5c3a07bfeaf1
kravebap <osef@osef.com>
2024-09-12 10:35:27 +0200

    sdqd

--- TP1/test.py
+++ TP1/test.py
@@ -9,5 +9,7 @@ def test_2():
 	assert f.puissance(-2, 3) == -8
 	assert f.puissance(-2, 2) == 4
 	assert f.puissance(2, -2) == 0.25
+
+def test_3():
 	with pytest.raises(ValueError):
 		f.puissance(0, -1)
