b85b03ed5f5f991e9f8a38a238685abcd3c97e01
kravebap <osef@osef.com>
2024-09-12 11:20:28 +0200

    dsqdqs

--- TP1/test.py
+++ TP1/test.py
@@ -2,14 +2,14 @@ import pytest
 import fonctions as f
 
 def test_1():
-	assert f.puissance(2, 3) == 8
-	assert f.puissance(2, 2) == 4
+	assert f.puissance(2.0, 3.0) == 8
+	assert f.puissance(2.0, 2.0) == 4
 
 def test_2():
-	assert f.puissance(-2, 3) == -8
-	assert f.puissance(-2, 2) == 4
-	assert f.puissance(2, -2) == 0.25
+	assert f.puissance(-2.0, 3.0) == -8
+	assert f.puissance(-2.0, 2.0) == 4
+	assert f.puissance(2.0, -2.0) == 0.25
 
 def test_4():
 	with pytest.raises(ValueError):
-		f.puissance(0, -1)
+		f.puissance(0.0, -1.0)
