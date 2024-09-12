24a33d66eee6beb85f14671793e23a31a51f3339
kravebap <osef@osef.com>
2024-09-12 09:03:51 +0200

    sdsdqdqs

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,7 +1,13 @@
 def puissance(a, b):
 	res = a
+
 	if type(a) is not int or type(b) is not int:
 		raise TypeError("pas entier")
-	for i in range(b-1):
-		res *= a
-	return res
+	if a == 0 and b < 0:
+		raise ValueError("0^-n invalide")
+
+	if b >= 0:
+		for i in range(b-1):
+			res *= a
+		return res
+	return 1/f.puissance(a, -b)
